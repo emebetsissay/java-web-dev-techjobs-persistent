a9d5f6b624212ee9629e101eb18f549367bbb7e3
emebetsissay <61718737+emebetsissay@users.noreply.github.com>
2020-07-15 17:00:00 -0500

    add queries.sql

--- queries.sql
+++ queries.sql
@@ -5,4 +5,6 @@ SELECT name FROM employer WHERE location="St. Louis City";
 ## Part 3: Test it with SQL
 DROP table job;
 ## Part 4: Test it with SQL
-
+SELECT s.name, s.description FROM skill s
+ INNER JOIN job_skills js ON s.id=js.skills_id
+ ORDER BY s.name;
